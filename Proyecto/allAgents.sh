638f367007ab6cd15e824842fe09c9060b9a4789
Jolicost <costasca6@gmail.com>
2018-06-10 15:34:59 +0200

    script para iniciar los agentes

--- Proyecto/allAgents.sh
+++ Proyecto/allAgents.sh
@@ -1,10 +1,11 @@
-python DirectorioAgentes.py &
+#!/usr/bin/env bash
 python AgenteAdmisor.py &
 python AgenteBuscador.py &
 python AgenteEmpaquetador.py &
-python AgenteEnviador.py &
 python AgenteOpinador.py &
 python AgenteReceptor.py &
 python AgenteUsuario.py &
 python AgenteVendedorExterno.py &
 python editorCentrosLogisticos.py &
+#Matar todos los procesos
+# pkill -P $$
